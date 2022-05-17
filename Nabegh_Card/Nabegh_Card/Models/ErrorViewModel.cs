2cfdf321975d971b8c921efe96ed954e1a08ce00
mehdinabegh <40233055+mehdinabegh@users.noreply.github.com>
2022-05-17 17:26:22 +0430

    Create Project

---
+++ Nabegh_Card/Nabegh_Card/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nabegh_Card.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
